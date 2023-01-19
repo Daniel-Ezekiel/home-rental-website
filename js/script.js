5575e2b544fb4dee72d459f246e87d15e127965a
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-01-19 04:12:02 +0100

    feat: implemented responsive mobile nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const btnHamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav");
+
+btnHamburger.addEventListener("click", function () {
+  menu.classList.toggle("active");
+  btnHamburger.classList.toggle("active");
+});
