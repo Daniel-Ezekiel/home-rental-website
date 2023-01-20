a45c7f9f5804bb10ad3a2003fa4411b827afa659
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-01-20 16:27:39 +0100

    feat: implemented file upload functionality to website

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-"use strict";
+'use strict';
 
-const btnHamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav");
+const btnHamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav');
+const fileUploader = document.querySelector('.upload-container');
 
-btnHamburger.addEventListener("click", function () {
-  menu.classList.toggle("active");
-  btnHamburger.classList.toggle("active");
+btnHamburger.addEventListener('click', function () {
+  menu.classList.toggle('active');
+  btnHamburger.classList.toggle('active');
+});
+
+fileUploader.addEventListener('click', function () {
+  const uploadInput = document.querySelector('#file-upload');
+
+  uploadInput.click();
+
+  uploadInput.addEventListener('change', function () {
+    console.log(this.files);
+    const uploadSuccess = document.createElement('span');
+    uploadSuccess.textContent = '✔ Photo(s) successfully uploaded';
+    uploadSuccess.classList.add('upload-success');
+    fileUploader.appendChild(uploadSuccess);
+  });
 });
